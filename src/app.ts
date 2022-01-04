5a9d415f9a4f350c5e98f8463cb3d96efe8abc9f
dkile <dkile532@naver.com>
2022-01-05 02:52:45 +0900

    specify port by process env

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response, NextFunction } from "express";
 
 const app = express();
+const PORT = process.env.PORT || 8010;
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("welcome!");
 });
 
-app.listen("8010", () => {
+app.listen(PORT, () => {
   console.log("8010 server listening");
 });
