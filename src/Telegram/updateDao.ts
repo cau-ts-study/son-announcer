83cbfc5ffd405535750fc89c2deae22a00d1d16b
LeoBang <bluke8489@gmail.com>
2022-01-31 21:12:17 +0900

    refac: type update

--- src/Telegram/updateDao.ts
+++ src/Telegram/updateDao.ts
@@ -1,40 +1,40 @@
-import { PoolConnection } from "mysql2/typings/mysql";
+import { PoolConnection } from "mysql2/promise";
 
-const getByChatId = (connection: PoolConnection, chatId: number) => {
+const getByChatId = async (connection: PoolConnection, chatId: number) => {
   const query = `
         SELECT chat_id 
         FROM Chat
         WHERE chat_id = ${chatId};
     `;
 
-  const chatIdRows = connection.query(query);
+  const chatIdRows = await connection.query(query);
   return chatIdRows;
 };
 
-const insertChatId = (connection: PoolConnection, chatId: number): void => {
+const insertChatId = async (connection: PoolConnection, chatId: number) => {
   const query = `
         INSERT INTO Chat (chat_id)
         VALUES (${chatId});
     `;
-  connection.query(query);
+  await connection.query(query);
 };
 
-const getAllChatId = (connection: PoolConnection) => {
+const getAllChatId = async (connection: PoolConnection) => {
   const query = `
         SELECT chat_id
         FROM Chat;
     `;
-  const chat_idRows = connection.query(query);
+  const chat_idRows = await connection.query(query);
   return chat_idRows;
 };
 
-const deleteChatId = (connection: PoolConnection, chat_id: number): void => {
+const deleteChatId = async (connection: PoolConnection, chat_id: number) => {
   const query = `
         DELETE FROM Chat
         WHERE chat_id = "${chat_id}";
     `;
 
-  connection.query(query);
+  await connection.query(query);
 };
 
 export default {
